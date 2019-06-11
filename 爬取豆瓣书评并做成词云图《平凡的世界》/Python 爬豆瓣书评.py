ddc69b1eeb3fa1e6e8866a458a0f37a8f727d491
Anokoro <15801755230@163.com>
2019-06-11 18:45:37 +0800

    Add files via upload

--- 爬取豆瓣书评并做成词云图《平凡的世界》/Python 爬豆瓣书评.py
+++ 爬取豆瓣书评并做成词云图《平凡的世界》/Python 爬豆瓣书评.py
@@ -17,7 +17,7 @@ limit = 1000  # 爬取前N条热门评论,
 # 第一步：爬取评论
 # 因为每一页只能爬取20条，所以我们用循环来模拟翻页
 all_comments = []
-for page in range(1,limit//20):  # 一页显示20条评论，
+for page in range(1,limit//19):  # 一页显示20条评论，
     # 用我们的学过的format来拼装URL
     print('正在爬取第{}页'.format(page))
     url = 'https://book.douban.com/subject/{}/comments/hot?p={}'.format(ID,page)
@@ -25,6 +25,7 @@ for page in range(1,limit//20):  # 一页显示20条评论，
     html_code = r.text
     soup = Soup(html_code, "html.parser")
     comments = soup.find(id='comments')  # 根据id 找到评论，，id在一个网页中是唯一的，就像我们的身份证号码一样
+
     # print(all_comments)
     # 然后从评论代码里面提取出评论的内容,
     comments = comments.find_all(name='span', attrs={'class': 'short'})
@@ -36,23 +37,17 @@ print('共爬取{}条评论'.format(len(all_comments))) # 看下总共爬下来�
 # 第二步：存到文件
 # 接下来把评论存到文件里面去，用我们学过的with语句
 filename = '{}评论.txt'.format(book_name)
-with open(filename,'w') as file:
+with open(filename,'w',encoding='utf-8') as file:
     print(all_comments)
     for comment in all_comments:
-        # 评论不一定会是纯文字的，可能会有一些表情，
-        # 如：写这么老多挺不容易的就多加一星吧🙄
-        # 这些表情会导致在写文件的时候编码错误,所以我们忽略这些评论
-        try:
-            print(comment)
-            str = '{}\n'.format(comment)
-            file.write(str)
-        except Exception as e:
-            continue
+        print(comment)
+        str = '{}\n'.format(comment)
+        file.write(str)
     print('评论已存到文件')
 
 # 第三步：读取文件，做成词云图
 # 最后我们从文件中读取出评论
-with open(filename,'r') as file:
+with open(filename,'r',encoding='utf-8') as file:
     comment = file.read()
     # 再调用词云分析程序，将评论做成一张词云图
     from create_wordcloud import gen_cloud
